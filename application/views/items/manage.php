79ec0893dec7d473f4195593287394f9745edce4
redmindtech <69637152+redmindtech@users.noreply.github.com>
2022-09-07 13:24:20 +0530

    Update manage.php

--- application/views/items/manage.php
+++ application/views/items/manage.php
@@ -4,6 +4,7 @@ $(document).ready(function()
 {
 	var receiving_quantity,items_add_quantity,items_less_quantity,item_id,final_val,i;
 	$('#table').hide();
+	$('#item_select').hide();
 	
     <?php $this->load->view('partial/bootstrap_tables_locale'); ?>
 
