76300f0152927e9fa45f25e5a9509c93c4e2f1d5
redmindtech <69637152+redmindtech@users.noreply.github.com>
2022-09-07 12:08:29 +0530

    Update manage.php

--- application/views/items/manage.php
+++ application/views/items/manage.php
@@ -69,6 +69,7 @@ $(document).ready(function()
             }).fail((jqXHR, errorMsg) => {
                 alert(jqXHR.responseText, errorMsg);
         });
+	 $('#register').DataTable().ajax.reload();
   
  });	 
 });
