b17bcf54da304f152244fd5132c1f8a712193b99
redmindtech <69637152+redmindtech@users.noreply.github.com>
2022-09-07 12:48:26 +0530

    Update manage.php

--- application/views/items/manage.php
+++ application/views/items/manage.php
@@ -139,7 +139,7 @@ $(document).ready(function()
                 <td><?php echo $item->pack_type ;?></td>
                 <td><?php  echo form_submit(array(
 				'name' => 'submit_qty',
-// 				'id' => 'submit_qty'.'<?php echo $item->item_id; ?>',
+// 				'id' => '<?php echo $item->item_id; ?>',
 				'value' => $this->lang->line('common_submit'),'class' => 'btn btn-primary btn-sm pull-right'));?></td>
                 <td></td>
                 <td></td>
