1349bfde8966bf0af8972033ddb257b222f350e8
redmindtech <69637152+redmindtech@users.noreply.github.com>
2022-09-22 10:50:50 +0530

    New Module added

--- application/language/en-US/module_lang.php
+++ application/language/en-US/module_lang.php
@@ -51,3 +51,4 @@ $lang["module_timeclocks"] = "";
 $lang["module_timeclocks_categories"] = "";
 $lang["module_timeclocks_categories_desc"] = "";
 $lang["module_timeclocks_desc"] = "";
+$lang["module_master"]="Master";
